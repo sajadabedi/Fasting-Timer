0d853782d8150ac5ae11fc4419f991d7ed66703d
Sajjad <sajad.abedi@veriff.net>
2022-09-17 13:09:23 +0300

    Initial Commit

---
+++ FastingTimer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FastingTimer
+//
+//  Created by Sajad Abedi on 17.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
